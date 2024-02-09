44dd1009a26451872058bb176593e7fe30653d2c
Wayne Chaw <wayne.chaw@gmail.com>
2024-02-09 06:35:33 -0800

    remove key

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ import { LolApi, RiotApi, Constants  } from 'twisted';
 let apikey = process.env.key;
 console.log(apikey);
 
-apikey = 'RGAPI-5d58b8f9-028a-4058-9785-307ee97ffd94';
+
 
 
 const api = new RiotApi({
