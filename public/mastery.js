447fd96f2152c960d8c12886d85c3d745239d2bd
Wayne Chaw <wayne.chaw@gmail.com>
2024-04-22 16:19:31 -0700

    token

--- public/mastery.js
+++ public/mastery.js
@@ -9847,30 +9847,44 @@ function getMasteryData(name, tag, selectedMappedRegion) {
 
         let donePercent = ((item.championPointsSinceLastLevel / totalPointsForNextLevel ) * 100) * 0.9;
 
+        let tokenColor = 'white';
+
+        if (item.championLevel == 5) {
+          tokenColor = '#ba68c8';
+        } else if (item.championLevel == 6) {
+          tokenColor = 'cyan';
+        }
+
+        let token = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="${tokenColor}" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+            <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
+          </svg>
+        `
+        
         let tokensHtml = `<div class="tokens-container"></div>`;
 
         if (item.tokensEarned == 1) {
           tokensHtml = 
           `
                   <div class="tokens-container">
-                    <i class="fa-solid fa-certificate"></i>
+                    ${token}
                   </div>
           `
         } else if (item.tokensEarned == 2) {
           tokensHtml = 
           `
                   <div class="tokens-container">
-                    <i class="fa-solid fa-certificate"></i>
-                    <i class="fa-solid fa-certificate"></i>
+                    ${token}
+                    ${token}
                   </div>
           `
         } else if (item.tokensEarned == 3) {
           tokensHtml = 
           `
                   <div class="tokens-container">
-                    <i class="fa-solid fa-certificate"></i>
-                    <i class="fa-solid fa-certificate"></i>
-                    <i class="fa-solid fa-certificate"></i>
+                    ${token}
+                    ${token}
+                    ${token}
                   </div>
           `
         }
@@ -10452,30 +10466,43 @@ console.log(hideM6);
 
         let donePercent = ((item.championPointsSinceLastLevel / totalPointsForNextLevel ) * 100) * 0.9;
 
+        let tokenColor = 'white';
+
+        if (item.championLevel == 5) {
+          tokenColor = '#ba68c8';
+        } else if (item.championLevel == 6) {
+          tokenColor = 'cyan';
+        }
+
+        let token = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="${tokenColor}" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+            <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
+          </svg>
+        `
         let tokensHtml = `<div class="tokens-container"></div>`;
 
         if (item.tokensEarned == 1) {
           tokensHtml = 
           `
                   <div class="tokens-container">
-                    <i class="fa-solid fa-certificate"></i>
+                    ${token}
                   </div>
           `
         } else if (item.tokensEarned == 2) {
           tokensHtml = 
           `
                   <div class="tokens-container">
-                    <i class="fa-solid fa-certificate"></i>
-                    <i class="fa-solid fa-certificate"></i>
+                    ${token}
+                    ${token}
                   </div>
           `
         } else if (item.tokensEarned == 3) {
           tokensHtml = 
           `
                   <div class="tokens-container">
-                    <i class="fa-solid fa-certificate"></i>
-                    <i class="fa-solid fa-certificate"></i>
-                    <i class="fa-solid fa-certificate"></i>
+                    ${token}
+                    ${token}
+                    ${token}
                   </div>
           `
         }
